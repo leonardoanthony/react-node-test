039e0cd1bcfee767e213c483b719a3c141fac357
leonardoanthony <leonardoanthony.dev@gmail.com>
2024-10-17 09:56:27 -0300

    feat: task2 - Complete CRUD in students-controler

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -3,26 +3,39 @@ const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updat
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
     //write your code
+    const payload = req.body;
+    const students = await getAllStudents(payload);
+    res.json({students});
 
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
     //write your code
+    const payload = req.body;
+    const response = await addNewStudent(payload);
+    res.json(response);
 
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
     //write your code
+    const payload = req.body;
+    const response = await updateStudent(payload)
+    res.json(response);
 
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
     //write your code
-
+    const {id} = req.params;
+    const student = await getStudentDetail(id);
+    res.json(student);
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
-    //write your code
+    const payload = req.body;
+    const response = await setStudentStatus(payload);
+    res.json(response);
 
 });
 
